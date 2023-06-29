3869a2ff7c371e014b53ce9949edc6799efedf06
jwbswll <boswelljackwilliam@gmail.com>
2023-06-29 15:08:30 +1000

    changes error handling so that incorrect morse characters get their own error rather than throwing the spaces error

--- modules/translator-functions.js
+++ modules/translator-functions.js
@@ -21,14 +21,13 @@ const morseToEnglish = (morse) => {
 	return morse
 		.split(" ")
 		.map((char) => {
-			if (char === "") {
+			if (char === "" || char === " ") {
 				throw new Error("Please only enter one space between morse characters");
 			} else if (char.length > 4) {
 				throw new Error("Morse characters need spaces between them");
-			} else if (!morseChars[char]) {
+			} else if (char.length <= 4 && !morseChars[char]) {
 				throw new Error("Please enter a valid Morse character");
-			}
-			return morseChars[char];
+			} else return morseChars[char];
 		})
 		.join("")
 		.toLowerCase();
